e74bdd52caf8afb3008a821f4b6dff225c808f26
rikelmy-matos <rikelmymatos07@gmail.com>
2024-12-13 01:15:14 -0300

    CRUD, Correções

--- lib/main.dart
+++ lib/main.dart
@@ -9,46 +9,32 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Agenda de Contatos',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(fontFamily: 'Roboto'),
+        ),
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.deepPurple,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Colors.deepPurple,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Agenda de Contatos'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -57,71 +43,404 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final LocalDatabase _database = LocalDatabase();
 
-  void _incrementCounter() {
+  List<Contact> _contacts = [];
+  List<Contact> _filteredContacts = [];
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadContacts();
+    _searchController.addListener(_filterContacts);
+  }
+
+  // Função para carregar todos os contatos do banco de dados
+  Future<void> _loadContacts() async {
+    List<Map<String, dynamic>> contactMaps = await _database.getUsers();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _contacts = contactMaps
+          .map((map) => Contact.fromMap(map))
+          .toList()
+        ..sort((a, b) => a.name.compareTo(b.name)); // Ordena os contatos por nome
+      _filteredContacts = List.from(_contacts); // Inicializa a lista filtrada
     });
+  }
+
+  // Função de pesquisa para filtrar os contatos
+  void _filterContacts() {
+    final query = _searchController.text.toLowerCase();
+    setState(() {
+      _filteredContacts = _contacts
+          .where((contact) =>
+      contact.name.toLowerCase().contains(query) ||
+          contact.phone.toLowerCase().contains(query))
+          .toList();
+    });
+  }
+
+  // Função para adicionar um novo contato
+  void _addContact(Contact contact) async {
+    await _database.insertUser(contact.toMap());
+    _loadContacts(); // Recarregar a lista após adicionar
+  }
+
+  // Função para editar um contato
+  void _editContact(BuildContext context, Contact contact) async {
+    final TextEditingController nameController = TextEditingController(text: contact.name);
+    final TextEditingController phoneController = TextEditingController(text: contact.phone);
+    final TextEditingController emailController = TextEditingController(text: contact.email);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Editar Contato'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: 'Nome'),
+              ),
+              TextField(
+                controller: phoneController,
+                decoration: const InputDecoration(labelText: 'Telefone'),
+                keyboardType: TextInputType.phone,
+              ),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email (opcional)'),
+                keyboardType: TextInputType.emailAddress,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (nameController.text.isNotEmpty && phoneController.text.isNotEmpty) {
+                  final updatedContact = Contact(
+                    id: contact.id, // Mantém o mesmo ID para atualizar
+                    name: nameController.text,
+                    phone: phoneController.text,
+                    email: emailController.text, // Atualiza o email
+                  );
+                  _database.updateUser(updatedContact); // Atualiza o contato no banco de dados
+                  _loadContacts(); // Recarregar a lista após editar
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Salvar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Função para excluir um contato
+  void _deleteContact(int id) async {
+    await _database.deleteUser(id);
+    _loadContacts(); // Recarregar a lista após excluir
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: ContactSearchDelegate(contacts: _contacts),
+              );
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Pesquisar Contato',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                prefixIcon: const Icon(Icons.search),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Expanded(
+            child: _filteredContacts.isEmpty
+                ? const Center(child: Text('Nenhum contato encontrado.'))
+                : ListView.builder(
+              itemCount: _filteredContacts.length,
+              itemBuilder: (context, index) {
+                final contact = _filteredContacts[index];
+                return Dismissible(
+                  key: Key(contact.id.toString()),
+                  onDismissed: (direction) {
+                    _deleteContact(contact.id);
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text('${contact.name} foi excluído')));
+                  },
+                  child: ListTile(
+                    title: Text(contact.name),
+                    subtitle: Text(contact.phone),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.edit),
+                          onPressed: () {
+                            _editContact(context, contact);
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () {
+                            _deleteContact(contact.id);
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text('${contact.name} foi excluído')));
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () => _showAddContactDialog(context),
+        tooltip: 'Adicionar Contato',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+
+  // Função para exibir a caixa de diálogo de adicionar contato
+  void _showAddContactDialog(BuildContext context) {
+    final TextEditingController nameController = TextEditingController();
+    final TextEditingController phoneController = TextEditingController();
+    final TextEditingController emailController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Adicionar Contato'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: 'Nome'),
+              ),
+              TextField(
+                controller: phoneController,
+                decoration: const InputDecoration(labelText: 'Telefone'),
+                keyboardType: TextInputType.phone,
+              ),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email (opcional)'),
+                keyboardType: TextInputType.emailAddress,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (nameController.text.isNotEmpty && phoneController.text.isNotEmpty) {
+                  final newContact = Contact(
+                    id: 0, // O ID será gerado automaticamente pelo banco de dados
+                    name: nameController.text,
+                    phone: phoneController.text,
+                    email: emailController.text, // Adiciona o campo email
+                  );
+                  _addContact(newContact);
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Adicionar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+// Classe Contact com método fromMap e toMap para mapear para o banco de dados
+class Contact {
+  final int id;
+  final String name;
+  final String phone;
+  final String email; // Campo de email opcional
+
+  Contact({required this.id, required this.name, required this.phone, required this.email});
+
+  // Método para criar um Contact a partir de um mapa (ex: do banco de dados)
+  factory Contact.fromMap(Map<String, dynamic> map) {
+    return Contact(
+      id: map['id'],
+      name: map['nome'],
+      phone: map['telefone'],
+      email: map['email'] ?? '', // Se não houver email, retorna uma string vazia
+    );
+  }
+
+  // Método para converter um Contact para um mapa (ex: para inserir no banco)
+  Map<String, dynamic> toMap() {
+    return {
+      'nome': name,  // O banco de dados não requer a inserção do 'id', pois é autoincrementado
+      'telefone': phone,
+      'email': email, // Adiciona o campo email
+    };
+  }
+}
+
+// Banco de dados para gerenciar os contatos
+class LocalDatabase {
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initializeDB('local.db');
+    return _database!;
+  }
+
+  Future<Database> _initializeDB(String filepath) async {
+    final dbpath = await getDatabasesPath();
+    final path = join(dbpath, filepath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future<void> _createDB(Database db, int version) async {
+    await db.execute('''CREATE TABLE user(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    telefone TEXT NOT NULL,
+    email TEXT
+    )''');
+  }
+
+  // Insert
+  Future<int> insertUser(Map<String, dynamic> user) async {
+    final db = await database;
+    return await db.insert('user', user);
+  }
+
+  // Read
+  Future<List<Map<String, dynamic>>> getUsers() async {
+    final db = await database;
+    return await db.query('user');
+  }
+
+  // Delete
+  Future<int> deleteUser(int id) async {
+    final db = await database;
+    return await db.delete('user', where: 'id = ?', whereArgs: [id]);
+  }
+
+  // Update
+  Future<int> updateUser(Contact contact) async {
+    final db = await database;
+    return await db.update(
+      'user',
+      contact.toMap(),
+      where: 'id = ?',
+      whereArgs: [contact.id],
+    );
+  }
+}
+
+class ContactSearchDelegate extends SearchDelegate {
+  final List<Contact> contacts;
+
+  ContactSearchDelegate({required this.contacts});
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final results = contacts
+        .where((contact) =>
+    contact.name.toLowerCase().contains(query.toLowerCase()) ||
+        contact.phone.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+
+    return ListView.builder(
+      itemCount: results.length,
+      itemBuilder: (context, index) {
+        final contact = results[index];
+        return ListTile(
+          title: Text(contact.name),
+          subtitle: Text(contact.phone),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestions = contacts
+        .where((contact) =>
+    contact.name.toLowerCase().contains(query.toLowerCase()) ||
+        contact.phone.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, index) {
+        final contact = suggestions[index];
+        return ListTile(
+          title: Text(contact.name),
+          subtitle: Text(contact.phone),
+        );
+      },
     );
   }
 }
